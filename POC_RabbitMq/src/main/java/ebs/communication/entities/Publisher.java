977b53555949b32fb76520253bc2aa51f8260722
PopescuAndreiGeorge <popescu.andreig333@gmail.com>
2024-06-08 15:51:26 +0300

    added protobuf

--- POC_RabbitMq/src/main/java/ebs/communication/entities/Publisher.java
+++ POC_RabbitMq/src/main/java/ebs/communication/entities/Publisher.java
@@ -1,9 +1,14 @@
 package ebs.communication.entities;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import ebs.communication.RabbitQueue;
 import ebs.communication.helpers.Tools;
 import ebs.generator.DBGenerator;
+import ebs.generator.entities.Publication;
+import org.example.protobuf.AddressBookProtos;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import static ebs.communication.entities.Constants.PUBLICATION_TYPE;
 
@@ -19,16 +24,47 @@ public class Publisher extends Thread  {
     public void run() {
 
         int numberOfPublications = 10;
-        var publications = DBGenerator.generatePublicationsList(numberOfPublications);
+        ArrayList<Publication> publications = DBGenerator.generatePublicationsList(numberOfPublications);
 
-        for (var pub : publications) {
+        ArrayList<AddressBookProtos.MessageWrapper> protoMessages = new ArrayList<>();
+        for (Publication pub : publications) {
+            AddressBookProtos.Publication.Builder protoPubBuilder = AddressBookProtos.Publication.newBuilder();
+            protoPubBuilder.setDrop(pub.getDrop());
+            protoPubBuilder.setValue(pub.getValue());
+            protoPubBuilder.setVariation(pub.getVariation());
+            protoPubBuilder.setDate(String.valueOf(pub.getDate()));
+            protoPubBuilder.setCompany(pub.getCompany());
+
+            AddressBookProtos.Publication protoPub = protoPubBuilder.build();
+            protoMessages.add(AddressBookProtos.MessageWrapper.newBuilder()
+                    .setTimestamp(System.currentTimeMillis())
+                    .setSource("publisher")
+                    .setType(PUBLICATION_TYPE)
+                    .setPublication(protoPub)
+                    .build());
+        }
+        for (AddressBookProtos.MessageWrapper message: protoMessages){
+            byte[] serializedMessage = message.toByteArray();
+            broker.sendMessage(serializedMessage);
+
+//            AddressBookProtos.MessageWrapper deserializedMessage = null;
+//            try {
+//                deserializedMessage = AddressBookProtos.MessageWrapper.parseFrom(serializedMessage);
+//            } catch (InvalidProtocolBufferException e) {
+//                throw new RuntimeException(e);
+//            }
+
+        }
+
+
+        for (Publication pub : publications) {
 
             JSONObject jsonObject = new JSONObject()
                     .put("type", PUBLICATION_TYPE)
                     .put("source", "publisher")
                     .put("message", pub.toJson().toString());
-
-            this.broker.sendMessage(jsonObject.toString());
+            //System.out.println("!!!!!!!!!" + jsonObject.toString());
+            //this.broker.sendMessage(jsonObject.toString());
         }
     }
 }
--- POC_RabbitMq/src/main/java/ebs/communication/entities/Subscriber.java
+++ POC_RabbitMq/src/main/java/ebs/communication/entities/Subscriber.java
@@ -1,10 +1,15 @@
 package ebs.communication.entities;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import ebs.communication.RabbitQueue;
 import ebs.communication.helpers.Tools;
 import ebs.generator.DBGenerator;
+import ebs.generator.entities.Subscription;
 import lombok.Getter;
+import org.example.protobuf.AddressBookProtos;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import static ebs.communication.entities.Constants.SUBSCRIPTION_TYPE;
 
@@ -57,17 +62,76 @@ public class Subscriber extends RabbitQueue {
         int nrDate = 2;
 
 
-        var subscriptions = DBGenerator.generateSubscriptionsList(companyEqualSign, numberOfSubscriptions, totalFields, nrCompany, nrValue, nrDrop, nrVariation, nrDate);
+        ArrayList<Subscription> subscriptions = DBGenerator.generateSubscriptionsList(companyEqualSign, numberOfSubscriptions, totalFields, nrCompany, nrValue, nrDrop, nrVariation, nrDate);
 
-        for (var sub : subscriptions) {
+        ArrayList<AddressBookProtos.MessageWrapper> protoMessages = new ArrayList<>();
+        for (Subscription sub : subscriptions){
+            AddressBookProtos.Subscription.Builder protoSubBuilder = AddressBookProtos.Subscription.newBuilder();
+            if(sub.getVariation() != null){
+                protoSubBuilder.setVariation(AddressBookProtos.SubscriptionFieldFloat.newBuilder()
+                        .setSign(sub.getVariation().getFirst())
+                        .setValue(sub.getVariation().getSecond()));
+            }
+            if(sub.getValue() != null){
+                protoSubBuilder.setValue(AddressBookProtos.SubscriptionFieldFloat.newBuilder()
+                        .setSign(sub.getValue().getFirst())
+                        .setValue(sub.getValue().getSecond()));
+            }
+            if(sub.getDrop() != null){
+                protoSubBuilder.setDrop(AddressBookProtos.SubscriptionFieldFloat.newBuilder()
+                        .setSign(sub.getDrop().getFirst())
+                        .setValue(sub.getDrop().getSecond()));
+            }
+            if(sub.getDate() != null){
+                protoSubBuilder.setDate(AddressBookProtos.SubscriptionFieldString.newBuilder()
+                        .setSign(sub.getDate().getFirst())
+                        .setValue(String.valueOf(sub.getDate().getSecond())));
+            }
+            if(sub.getCompany() != null){
+                protoSubBuilder.setCompany(AddressBookProtos.SubscriptionFieldString.newBuilder()
+                        .setSign(sub.getCompany().getFirst())
+                        .setValue(sub.getCompany().getSecond()));
+            }
+            AddressBookProtos.Subscription protoSub = protoSubBuilder.build();
+
+            protoMessages.add(AddressBookProtos.MessageWrapper.newBuilder()
+                    .setType(SUBSCRIPTION_TYPE)
+                    .setSource(name)
+                    .setTimestamp(System.currentTimeMillis())
+                    .setSubscription(protoSub)
+                    .build());
+            //System.out.println(sub.getVariation());
+        }
+//
+        for (AddressBookProtos.MessageWrapper message: protoMessages){
+            byte[] serializedMessage = message.toByteArray();
+            broker.sendMessage(serializedMessage);
+
+//            AddressBookProtos.MessageWrapper deserializedMessage = null;
+//            try {
+//                deserializedMessage = AddressBookProtos.MessageWrapper.parseFrom(serializedMessage);
+//            } catch (InvalidProtocolBufferException e) {
+//                throw new RuntimeException(e);
+//            }
+
+//            System.out.println("Message company: " + deserializedMessage.getSubscription().getCompany());
+//            System.out.println("Company: " + message.getSubscription().getCompany());
+//            System.out.println("Date: " + message.getSubscription().getDate());
+//            System.out.println("Drop: " + message.getSubscription().getDrop());
+//            System.out.println("Value: " + message.getSubscription().getValue());
+//            System.out.println("Variation: " + message.getSubscription().getVariation());
+        }
+
+
+        for (Subscription sub : subscriptions) {
             JSONObject jsonObject = new JSONObject()
                     .put("type", SUBSCRIPTION_TYPE)
                     .put("source", name)
                     .put("message", sub.toJson().toString());
 
-//            System.out.println(jsonObject.toString());
-
-            broker.sendMessage(jsonObject.toString());
+            //System.out.println(jsonObject.toString());
+            //System.out.println(jsonObject.getString("source"));
+            //broker.sendMessage(jsonObject.toString());
         }
     }
 }
